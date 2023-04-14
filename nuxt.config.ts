8af6418af249cfd709679c304349e8db85190284
Klein Petr <petr@kleinpetr.com>
2023-04-14 18:38:30 +0200

    add linter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        '@nuxt/devtools',
-        '@unocss/nuxt',
-        '@vueuse/nuxt',
-    ],
-    css: [
-        '@unocss/reset/tailwind.css',
-    ],
+  modules: [
+    '@nuxt/devtools',
+    '@unocss/nuxt',
+    '@vueuse/nuxt',
+  ],
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
 })
